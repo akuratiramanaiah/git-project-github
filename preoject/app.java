978a61d8ddb1fd12f7b6c7e245e1b3eaae4da5d3
Ramanaiah <akuratiramanaiah@gmail.com>
2026-09-06 05:08:26 +0000

    java code commited

---
+++ preoject/app.java
@@ -0,0 +1,13 @@
+public calss App {
+	public static void main (string[] args) {
+		init a=10;
+		init b=5;
+		system.out.println ("the sum is:"+(a+b);
+	        system.out.println ("the sub is:"+(a-b);
+		system.out.println ("the mult is:"+a*b);
+		system.out.println ("the division is:"+(a/b);
+	}
+}
+
+
+
